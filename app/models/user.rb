a4eec9896ff3926b6f5c989f7e4538a43da39934
Amanda Ross <amikaross@gmail.com>
2023-01-17 07:24:55 -0700

    Add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :favorites 
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe User, type: :model do
   describe 'relationships' do 
     it { should have_many(:favorites) }
   end
+
+  describe 'validations' do 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
